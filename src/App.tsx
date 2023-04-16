a8aa220b25c25561eb499335371a81500ec978ca
abhigattineni <abhishekgattineni@gmail.com>
2023-04-16 09:20:23 -0700

    changes on 4/16/2023

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import { useState } from "react";
 import ExpandableText from "./components/ExpandableText/ExpandableText";
 import Form from "./components/Form/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import HorizontalLinearStepper from "./components/Stepper/Stepper";
 
 function App() {
   // const [game, setGame] = useState({
@@ -16,21 +19,39 @@ function App() {
   //     name: "John",
   //   },
   // });
-  const [expenses, setExpense] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "utilities" },
-    { id: 2, description: "bbb", amount: 10, category: "utilities" },
-    { id: 3, description: "ccc", amount: 10, category: "utilities" },
-    { id: 4, description: "ddd", amount: 10, category: "utilities" },
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
   ]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((expense) => expense.category === selectedCategory)
+    : expenses;
 
   // const onChange = () => {
   //   setGame({ ...game, player: { name: "reddy" } });
   // };
   return (
-    <ExpenseList
-      expenses={expenses}
-      onDelete={(id) => setExpense(expenses.filter((e) => e.id !== id))}
-    />
+    <div>
+      {/* <HorizontalLinearStepper /> */}
+      <div className="mb-3">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter onSelectCategory={(e) => setSelectedCategory(e)} />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </div>
   );
 }
 
