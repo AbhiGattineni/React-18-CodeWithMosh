33da430a6dd340f5f4d6660d0a46af6b16c4ef33
abhigattineni <abhishekgattineni@gmail.com>
2023-04-28 06:15:04 -0700

    game card completed

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,10 @@
 import axios,{CanceledError} from 'axios';
 
 export default axios.create({
-    baseURL: 'https://jsonplaceholder.typicode.com'
+    baseURL: 'https://api.rawg.io/api',
+    params: {
+        key: '07475b09680642a982c574f220fd7b17'
+    }
 })
 
 export {CanceledError};
